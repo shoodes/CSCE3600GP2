a27c8db02048890eb154195a4c41c06188ac3c42
SinisterNight <114369787+SinisterNight@users.noreply.github.com>
2023-04-21 18:28:19 -0500

    Update Path.c
    comments

--- Path.c
+++ Path.c
@@ -5,126 +5,118 @@
 #include <assert.h>
 #include "Path.h"
 
-static char paths[MAX_PATHS][MAX_PATH_LENGTH];
-static int num_paths;
+static char paths[MAX_PATHS][MAX_PATH_LENGTH]; //max paths and max path lengths
+static int num_paths; // num paths
 
-void init_path() {
-    char *path = getenv("PATH");
-    if (path == NULL) {
-        printf("Failed to get PATH environment variable\n");
-        exit(1);
+void init_path() { //init path
+    char *path = getenv("PATH"); //setting path
+    if (path == NULL) { // if path is null
+        printf("PATH FAIL"); // print path fail
+        exit(1);//exit program
+    }//end if
+
+    char *path_copy = strdup(path); //path copy equals strdup path
+    if (path_copy == NULL) { //path copy conditional 
+        perror("Failed to allocate memory");//perror
+        exit(1);//exit
+    } //end if
+
+    char *token = strtok(path_copy, ":"); //token =equals strtok path copy mem alloc
+
+    while (token != NULL && num_paths < MAX_PATHS) { //run loop while path is still less than max path
+        strcpy(paths[num_paths], token); //copy paths num paths , token
+        token = strtok(NULL, ":");//set token
+        num_paths++; //paths increment
+    }// end while loop
+
+
+    free(path_copy); //free up path copy
+} // end init
+void print_path() {  //print path
+    printf("Path: "); //print
+
+    for (int i = 0; i < num_paths; i++) { //running through path
+        printf("Printing path %d\n", i); //print
+        printf("%s", paths[i]); //print paths i
+        if (i != num_paths - 1) { //value if i  not equals -1
+            printf(":"); //print
+        }// end if
     }
-
-   // printf("Original PATH: %s\n", path); PATH DEBUGGING STATEMENTS
-
-    char *path_copy = strdup(path);
-    if (path_copy == NULL) {
-        perror("Failed to allocate memory");
-        exit(1);
-    }
-
-    //printf("PATH copy: %s\n", path_copy); PATH DEBUGGING STATEMENTS
-
-    char *token = strtok(path_copy, ":");
-
-    while (token != NULL && num_paths < MAX_PATHS) {
-        //printf("Adding path: %s\n", token); PATH DEBUGGING STATEMENTS
-        strcpy(paths[num_paths], token);
-        token = strtok(NULL, ":");
-        num_paths++;
-    }
-
-   // printf("Finished initializing paths\n"); PATH DEBUGGING STATEMENTS
-
-    free(path_copy);
+    printf("\n");//new line
 }
-void print_path() {
-    printf("In print_path\n");
-    printf("Path: ");
-
-    for (int i = 0; i < num_paths; i++) {
-        printf("Printing path %d\n", i);
-        printf("%s", paths[i]);
-        if (i != num_paths - 1) {
-            printf(":");
-        }
+void append_path(char *path) { //appends path
+    if (num_paths == MAX_PATHS) { // if num path equals max
+        printf("Max Paths reached\n"); //max paths
+        return; //return
     }
-    printf("\n");
-}
-void append_path(char *path) {
-    if (num_paths == MAX_PATHS) {
-        printf("Cannot add path, maximum number of paths reached\n");
-        return;
+    if (strlen(path) >= MAX_PATH_LENGTH) { //if len > max path length
+        printf("Cannot add path, path too long\n"); //too  long
+        return;//return
     }
-    if (strlen(path) >= MAX_PATH_LENGTH) {
-        printf("Cannot add path, path too long\n");
-        return;
-    }
-    strncpy(paths[num_paths], path, MAX_PATH_LENGTH);
-    paths[num_paths][MAX_PATH_LENGTH - 1] = '\0';
-    num_paths++;
+    strncpy(paths[num_paths], path, MAX_PATH_LENGTH); //cpy num_paths, paths, max path lengtth
+    paths[num_paths][MAX_PATH_LENGTH - 1] = '\0'; //null character
+    num_paths++;//increment path
 }
 
 
-void remove_path(char *path) {
-    int index = -1;
-    for (int i = 0; i < num_paths; i++) {
-        if (strcmp(paths[i], path) == 0) {
-            index = i;
-            break;
-        }
-    }
+void remove_path(char *path) { //remove path
+    int index = -1; //index -1
+    for (int i = 0; i < num_paths; i++) { //iteriate num paths
+        if (strcmp(paths[i], path) == 0) { //strcmp paths i. path equals 0
+            index = i; //set index to i
+            break;//break loop
+        }//end if
+    }// end for
 
-    if (index == -1) {
-        printf("Cannot remove path, path not found\n");
-        return;
-    }
+    if (index == -1) { //if index equals -1
+        printf("Cannot remove path, path not found\n"); // print not found
+        return; // return
+    }// end if
 
-    for (int i = index; i < num_paths - 1; i++) {
-        strcpy(paths[i], paths[i + 1]);
+    for (int i = index; i < num_paths - 1; i++) { //loop
+        strcpy(paths[i], paths[i + 1]); //cpy paths i, paths i+1
     }
-    num_paths--;
+    num_paths--; //num paths dec
 }
 
 char* get_full_path(char *command) {
-    char *full_path = malloc(MAX_PATH_LENGTH + strlen(command) + 2);
-    if (full_path == NULL) {
-        perror("Failed to allocate memory");
-        exit(1);
+    char *full_path = malloc(MAX_PATH_LENGTH + strlen(command) + 2); //malloc
+    if (full_path == NULL) {//check if path null
+        perror("Failed to allocate memory");//if goes wrong
+        exit(1);//exit
     }
 
-    for (int i = 0; i < num_paths; i++) {
-        sprintf(full_path, "%s/%s", paths[i], command);
-        if (access(full_path, X_OK) == 0) {
-            return full_path;
-        }
-    }
+    for (int i = 0; i < num_paths; i++) { //iterate num paths
+        sprintf(full_path, "%s/%s", paths[i], command); //print full path, format, paths i, command
+        if (access(full_path, X_OK) == 0) { // access  full path/ exec
+            return full_path; // return path
+        }//end if
+    }//end for
 
-    free(full_path);
-    return NULL;
-}
+    free(full_path); //free mem
+    return NULL; // return null
+}//end get full path
 
-void handle_path_command(char **args) {
-    //printf("handle_path_command: Handling path command\n"); PATH DEBUGGING STATEMENTS
-    if (args[1] == NULL) {
-        printf("handle_path_command: No arguments\n");
-        print_path();
-    } else if (strcmp(args[1], "+") == 0) {
-        printf("handle_path_command: Adding path\n");
-        if (args[2] == NULL) {
-            printf("Invalid arguments\n");
-            return;
+void handle_path_command(char **args) {//start handle path
+    if (args[1] == NULL) { //check args
+        printf("No path\n");
+        print_path();//print path 
+    } else if (strcmp(args[1], "+") == 0) { // check if arg +
+        printf("Adding path\n"); //add path
+        if (args[2] == NULL) {//checl arg 2 null
+            printf("Invalid path\n"); //invalid args
+            return;// return
         }
-        append_path(args[2]);
-    } else if (strcmp(args[1], "-") == 0) {
-        printf("handle_path_command: Removing path\n");
-        if (args[2] == NULL) {
-            printf("Invalid arguments\n");
-            return;
+        append_path(args[2]);//append path
+    } else if (strcmp(args[1], "-") == 0) {//stringcmp condition w args 1
+        printf("Removing path\n"); //print handle path
+        if (args[2] == NULL) {//args 2 condition
+            printf("Invalid path\n"); //printf
+            return;//return
         }
-        remove_path(args[2]);
-    } else {
-        printf("Invalid arguments\n");
-        return;
+        remove_path(args[2]); //remove path args 2
+    } else {//else statement
+        printf("Invalid path\n");//print
+        return;//return
     }
 }
